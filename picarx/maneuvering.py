3e21a3db007899e694fbf2f6757a7291ad36c7d9
Marcus Rosette <rosettem@oregonstate.edu>
2023-01-20 14:41:28 -0800

    added additional variable call

--- picarx/maneuvering.py
+++ picarx/maneuvering.py
@@ -2,9 +2,11 @@ import time
 import numpy as np
 from picarx import Picarx
 
+# marcus@picar14.engr.oregonstate.edu
+
 def forward_to_backward(speed, steering_angle, action_time):
     movement = Picarx()
-    movement.set_motor_speed(speed)
+    movement.set_motor_speed(speed=speed)
     movement.set_wheel_vel_scale(steering_angle)
     movement.set_dir_servo_angle(steering_angle)
 
