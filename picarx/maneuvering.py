d4da36b58e5031a5f4452210762f0157026b0906
Marcus Rosette <rosettem@oregonstate.edu>
2023-01-20 15:21:50 -0800

    set both motor speeds and added stop function

--- picarx/maneuvering.py
+++ picarx/maneuvering.py
@@ -1,12 +1,14 @@
 import time
 import numpy as np
-from picarx import Picarx
+from picarx_improved import Picarx
 
 # marcus@picar14.engr.oregonstate.edu
 
 def forward_to_backward(speed, steering_angle, action_time):
     movement = Picarx()
-    movement.set_motor_speed(speed=speed)
+    movement.set_motor_speed(1, speed)
+    movement.set_motor_speed(2, speed)
+    # movement.motor_direction_calibration()
     movement.set_wheel_vel_scale(steering_angle)
     movement.set_dir_servo_angle(steering_angle)
 
@@ -15,16 +17,11 @@ def forward_to_backward(speed, steering_angle, action_time):
     while time.time() < end_time:
         movement.forward(speed)
 
-    while time.time() < end_time:
-        movement.backward(speed)
+    # while time.time() < end_time:
+    #     movement.backward(speed)
 
+    movement.stop()
 
 if __name__ == "__main__":
-    try:
-        forward_to_backward(30, 0, 2)
-        time.sleep(0.5)
+    forward_to_backward(30, 0, 2)
 
-    finally:
-        # px.forward(0)
-        Picarx.stop()
-        time.sleep(.2)
