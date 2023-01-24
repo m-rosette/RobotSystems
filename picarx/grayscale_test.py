2260fe260b25c1b18f144283863a8e80c9eb4533
Marcus Rosette <rosettem@oregonstate.edu>
2023-01-24 13:00:32 -0800

    fixing another class error

--- picarx/grayscale_test.py
+++ picarx/grayscale_test.py
@@ -8,7 +8,7 @@ import time
 if __name__ == '__main__':
     while 1:
         sensor = Picarx()
-        gray_data = sensor.Picarx.get_grayscale_data()
+        gray_data = sensor.get_grayscale_data()
         print(gray_data)
         print(sensor.get_line_status(gray_data))
         time.sleep(0.1)
