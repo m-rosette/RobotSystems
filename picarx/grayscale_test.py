306230954d635737608592ced30ad2c7eb7deaac
Marcus Rosette <rosettem@oregonstate.edu>
2023-01-26 13:41:44 -0800

    only normalizing gray scale test file

--- picarx/grayscale_test.py
+++ picarx/grayscale_test.py
@@ -3,13 +3,16 @@
 
 from picarx_improved import Picarx
 import time
+import numpy as np
 
 
 if __name__ == '__main__':
     while 1:
         sensor = Picarx()
         gray_data = sensor.get_grayscale_data()
+        np_array = np.array(gray_data)
+        normalized = np.linalg.norm(np_array)
         print(gray_data)
-        print(sensor.get_line_status(gray_data))
+        # print(sensor.get_line_status(gray_data))
         time.sleep(0.5)
 
