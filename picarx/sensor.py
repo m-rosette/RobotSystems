bcacc22057d5fdc844f4b5d5c8cd57ddce9f9164
Marcus Rosette <rosettem@oregonstate.edu>
2023-01-31 13:43:49 -0800

    GrayscaleSensor inheriting from Grayscale_Module

--- picarx/sensor.py
+++ picarx/sensor.py
@@ -1,21 +1,21 @@
 import picarx_improved as px
 from picarx_improved import Grayscale_Module
 import time
+from sim_robot_hat import ADC
 
 
 class GrayscaleSensor(Grayscale_Module):
     def __init__(self):
-        px()
-        # self.chn_0 = ADC("A0")
-        # self.chn_1 = ADC("A1")
-        # self.chn_2 = ADC("A2")
+        self.chn_0 = ADC("A0")
+        self.chn_1 = ADC("A1")
+        self.chn_2 = ADC("A2")
 
-    # def get_grayscale_data(self):
-    #     adc_value_list = []
-    #     adc_value_list.append(self.chn_0.read())
-    #     adc_value_list.append(self.chn_1.read())
-    #     adc_value_list.append(self.chn_2.read())
-    #     return adc_value_list
+    def get_grayscale_data(self):
+        adc_value_list = []
+        adc_value_list.append(self.chn_0.read())
+        adc_value_list.append(self.chn_1.read())
+        adc_value_list.append(self.chn_2.read())
+        return adc_value_list
 
     def sensor_com(self, communication, delay):
         while True:
