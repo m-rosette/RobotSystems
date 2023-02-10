204d183e83f4f87a2a396a05a4649008f4933fe4
Marcus Rosette <rosettem@oregonstate.edu>
2023-02-10 09:59:20 -0800

    adding additional sensor call in interpreter

--- picarx/bus.py
+++ picarx/bus.py
@@ -39,7 +39,7 @@ class Exexute():
                 eGrayscale = executor.submit(self.sensor.grayscale_bus, self.grayscale_bus, delay)
                 eUltrasonic = executor.submit(self.sensor.ultrasonic_bus, self.ultrasonic_bus, delay)
                 eCamera = executor.submit(self.sensor.camera_bus, self.camera_bus, delay)
-                eInterpreter = executor.submit(self.interpreter.interpreter_bus, self.grayscale_bus, self.interpreter, delay)
+                eInterpreter = executor.submit(self.interpreter.interpreter_bus, self.grayscale_bus, self.camera_bus, self.line_bus, delay)
                 eController = executor.submit(self.controller.controller_bus, self.line_bus, self.ultrasonic_bus, delay)
 
             eGrayscale.result()
