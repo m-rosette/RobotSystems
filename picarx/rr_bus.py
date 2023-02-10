b837a20442ab11887485962b4924b7e05042d79e
Marcus Rosette <rosettem@oregonstate.edu>
2023-02-10 14:39:46 -0800

    changing the number of input buses in consumer producer

--- picarx/rr_bus.py
+++ picarx/rr_bus.py
@@ -78,7 +78,7 @@ read_camera = rr.Producer(
 # Wrap the multiplier function into a consumer-producer
 interp_grayscale = rr.ConsumerProducer(
     (interpreter.detect_edge, camera_interp.camera_line_follow),  # function that will process data
-    (grayscale_bus, camera_bus, ultrasonic_bus),  # input data buses
+    (grayscale_bus, camera_bus),  # input data buses
     interp_bus,  # output data bus
     delay,  # delay between data control cycles
     terminator_bus,  # bus to watch for termination signal
