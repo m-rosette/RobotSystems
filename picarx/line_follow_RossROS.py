73fae3d8b5549fa6ad313b14a000e0f82e0ffe30
Marcus Rosette <rosettem@oregonstate.edu>
2023-02-10 11:18:26 -0800

    inital trial of RossROS implementation on graysensor

---
+++ picarx/line_follow_RossROS.py
@@ -0,0 +1,55 @@
+import sys
+sys.path.append(r'/home/marcus/RobotSystems/RossROS')
+from RossROS import Bus, ConsumerProducer, Consumer, Producer, Timer
+import logging
+import time
+import math
+from sensor import Sensor
+from interpreter import Interpreter
+from controller import Controller
+from picarx_improved import Picarx
+import concurrent.futures
+from readerwriterlock import rwlock
+
+
+class RossROS_Exexute():
+    def __init__(self) -> None:
+        self.px = Picarx()
+        self.sensor = Sensor()
+        self.interpreter = Interpreter()
+        self.controller = Controller(self.px)
+
+        self.grayscale_bus = Bus(name="gray bus")
+        self.ultrasonic_bus = Bus(name="ultrasonic bus")
+        self.camera_bus = Bus("camera bus")
+        self.interp_bus = Bus(name="interpreter bus")
+        self.terminator_bus = Bus(name="termination bus")
+
+    def execute(self):
+        while True:
+            timer = Timer(self.terminator_bus, 1, 0.01, self.terminator_bus)
+            sensor_producer = Producer(self.sensor, output_busses=self.grayscale_bus, delay=0.1, termination_busses=self.terminator_bus, name="sensor producer")
+            interpreter_cons_prod = ConsumerProducer(self.interpreter, output_busses=self.interp_bus, delay=0.1, termination_busses=self.terminator_bus, name="interpreter consumer producer")
+            controller_consumer = Consumer(self.controller, input_busses=(self.interp_bus, self.ultrasonic_bus), delay=0.1, termination_busses=self.terminator_bus, name="sensor producer")
+
+            with concurrent.futures.ThreadPoolExecutor(max_workers = 3) as executor:
+                eGrayscale = executor.submit(sensor_producer)
+                eInterpreter = executor.submit(interpreter_cons_prod)
+                eController = executor.submit(controller_consumer)
+                eTimer = executor.submit(timer)
+
+            eGrayscale.result()
+            eInterpreter.result()
+            eController.result()
+            eTimer.result()
+
+
+if __name__ == '__main__':
+    executor = RossROS_Exexute()
+    executor.executor()
+
+
+
+
+
+
