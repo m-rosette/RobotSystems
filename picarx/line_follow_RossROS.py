3b07f7a99e5657a5532fef7754d3a90794116a19
Marcus Rosette <rosettem@oregonstate.edu>
2023-02-10 12:21:14 -0800

    changing gray sensor bus setup

--- picarx/line_follow_RossROS.py
+++ picarx/line_follow_RossROS.py
@@ -15,7 +15,7 @@ from readerwriterlock import rwlock
 class RossROS_Exexute():
     def __init__(self) -> None:
         self.px = Picarx()
-        self.sensor = Sensor()
+        
         # self.gray_sensor = self.sensor.grayscale_bus
         self.interpreter = Interpreter()
         self.controller = Controller(self.px)
@@ -25,11 +25,13 @@ class RossROS_Exexute():
         self.camera_bus = Bus("camera bus")
         self.interp_bus = Bus(name="interpreter bus")
         self.terminator_bus = Bus(name="termination bus")
-
+        
+        self.sensor = Sensor()
+        self.gray_sensor = self.sensor.grayscale_bus(self.grayscale_bus, 0.1)
     def execute(self):
         while True:
             timer = Timer(self.terminator_bus, 1, 0.01, self.terminator_bus)
-            sensor_producer = Producer(self.sensor.grayscale_bus, output_buses=self.grayscale_bus, delay=0.1, termination_buses=self.terminator_bus, name="sensor producer")
+            sensor_producer = Producer(self.gray_sensor, output_buses=self.grayscale_bus, delay=0.1, termination_buses=self.terminator_bus, name="sensor producer")
             interpreter_cons_prod = ConsumerProducer(self.interpreter, input_buses=self.grayscale_bus, output_buses=self.interp_bus, delay=0.1, termination_buses=self.terminator_bus, name="interpreter consumer producer")
             controller_consumer = Consumer(self.controller, input_buses=(self.interp_bus, self.ultrasonic_bus), delay=0.1, termination_buses=self.terminator_bus, name="sensor producer")
 
