98de90f6cef9d00bf53f2adb9ca485dba553dfdd
Marcus Rosette <rosettem@oregonstate.edu>
2023-02-10 12:00:36 -0800

    calling grayscale sensor

--- picarx/line_follow_RossROS.py
+++ picarx/line_follow_RossROS.py
@@ -16,6 +16,7 @@ class RossROS_Exexute():
     def __init__(self) -> None:
         self.px = Picarx()
         self.sensor = Sensor()
+        self.gray_sensor = self.sensor.grayscale_bus
         self.interpreter = Interpreter()
         self.controller = Controller(self.px)
 
@@ -28,7 +29,7 @@ class RossROS_Exexute():
     def execute(self):
         while True:
             timer = Timer(self.terminator_bus, 1, 0.01, self.terminator_bus)
-            sensor_producer = Producer(self.sensor, output_buses=self.grayscale_bus, delay=0.1, termination_buses=self.terminator_bus, name="sensor producer")
+            sensor_producer = Producer(self.gray_sensor, output_buses=self.grayscale_bus, delay=0.1, termination_buses=self.terminator_bus, name="sensor producer")
             interpreter_cons_prod = ConsumerProducer(self.interpreter, input_buses=self.grayscale_bus, output_buses=self.interp_bus, delay=0.1, termination_buses=self.terminator_bus, name="interpreter consumer producer")
             controller_consumer = Consumer(self.controller, input_buses=(self.interp_bus, self.ultrasonic_bus), delay=0.1, termination_buses=self.terminator_bus, name="sensor producer")
 
